ac8951d3e0d88c87c2a3edf1d46a13486e105f39
周正文 <b23583795@gmail.com>
2019-03-11 13:22:37 +0800

    lager buttom

--- Test0311_Hello/Test0311_Hello/Form1.Designer.cs
+++ Test0311_Hello/Test0311_Hello/Form1.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(248, 57);
+            this.button1.Location = new System.Drawing.Point(198, 133);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(293, 90);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
