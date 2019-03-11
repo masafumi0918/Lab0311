2123167a160f8d2ad777bf4467a4cb3875b26488
周正文 <b23583795@gmail.com>
2019-03-11 13:51:44 +0800

    update coment on buttom2 branch

--- Test0311_Hello/Test0311_Hello/Form1.cs
+++ Test0311_Hello/Test0311_Hello/Form1.cs
@@ -25,6 +25,7 @@ namespace Test0311_Hello
         private void button2_Click(object sender, EventArgs e)
         {
             button2.Text = DateTime.Now.ToString();
+            //update coment on buttom2 branch
         }
     }
 }
